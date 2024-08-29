97ff3c3401dd9aea17912ad0894dbfeb9ffde26e
Matheusalves112 <113462888+Matheusalves112@users.noreply.github.com>
2024-08-29 14:00:31 -0300

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Cafeteria</h1>
+      <p>Discover our delicious menu and cozy atmosphere.</p>
+    </div>
+  );
+};
+
+export default Home;
+
